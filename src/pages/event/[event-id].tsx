991409cb9b630ff06175303f5570073a440c7c68
RubensJr21 <junior.correa2001@gmail.com>
2024-04-15 17:14:12 -0300

    Adiciona o estilo da página principal e adiciona telas do evento

---
+++ src/pages/event/[event-id].tsx
@@ -0,0 +1,7 @@
+export default function EventID(){
+    return (
+        // <Icone da aplicação />
+        <h1>Página do evento</h1>
+        // Caso o id fornecido seja inválido será redirecionado para a tela de criação
+    )
+}
--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,6 +1,6 @@
 export default function Event(){
     return (
         // <Icone da aplicação />
-        <h1>Nome do evento</h1>
+        <h1>Aqui você criará um novo evento</h1>
     )
 }
